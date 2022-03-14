d69f8fe8e20c51f2b752f1b8eba7d035b7d7491e
Pedro Paulo <pedropaulotj@gmail.com>
2022-03-13 21:17:12 -0300

    fix: Change the app new version

--- src/screens/Configuracoes/index.js
+++ src/screens/Configuracoes/index.js
@@ -533,7 +533,7 @@ export default function Configuracoes({ navigation }) {
                 <ItemListTextDescription
                   onPress={() => setObgEscondido((o) => o + 1)}
                 >
-                  {t("screens.configuracoes.version")} 1.0.12
+                  {t("screens.configuracoes.version")} 1.0.13
                 </ItemListTextDescription>
               </ItemListTitleSpace100>
             </ItemListSpaceBetween>
