cf76d950d13c837cf4c4311bc54aa13265687856
Pedro Paulo <pedropaulotj@gmail.com>
2022-09-10 12:55:25 -0300

    feat: Updated app version to 1.1.4 (23)

--- src/screens/Configuracoes/index.tsx
+++ src/screens/Configuracoes/index.tsx
@@ -592,7 +592,7 @@ export default function Configuracoes({ navigation }: Props) {
                 <ItemListTextDescription
                   onPress={() => setObgEscondido((o) => o + 1)}
                 >
-                  {t("screens.configuracoes.version")} 1.1.2
+                  {t("screens.configuracoes.version")} 1.1.4
                 </ItemListTextDescription>
               </ItemListTitleSpace100>
             </ItemListSpaceBetween>
