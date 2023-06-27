f694e64bead855b6f03b9bec3f11781e94e37854
pedropaulodf <pedropaulotj@gmail.com>
2023-06-26 22:19:11 -0300

    fix: Centered menu text in home screen

--- src/screens/Home/styles.ts
+++ src/screens/Home/styles.ts
@@ -1,16 +1,16 @@
-import styled from 'styled-components/native';
-import { ResponsiveSize } from '../../utils/fontResponsive'
-import { TouchableOpacity } from 'react-native';
+import styled from "styled-components/native";
+import { ResponsiveSize } from "../../utils/fontResponsive";
+import { TouchableOpacity } from "react-native";
 
-import IconTerritoriosSvg from '../../images/svg/IconTerritoriosSvg';
-import IconPessoasSvg from '../../images/svg/IconPessoasSvg';
-import IconRelatoriosSvg from '../../images/svg/IconRelatoriosSvg';
-import IconCronometroSvg from '../../images/svg/IconCronometroSvg';
+import IconTerritoriosSvg from "../../images/svg/IconTerritoriosSvg";
+import IconPessoasSvg from "../../images/svg/IconPessoasSvg";
+import IconRelatoriosSvg from "../../images/svg/IconRelatoriosSvg";
+import IconCronometroSvg from "../../images/svg/IconCronometroSvg";
 
 export const Container = styled.View`
-  background-color: ${({theme}) => theme.color.bg};
+  background-color: ${({ theme }) => theme.color.bg};
   padding: 15px;
-  height: 100%; 
+  height: 100%;
   flex-direction: column;
   justify-content: center;
   align-items: center;
@@ -18,7 +18,7 @@ export const Container = styled.View`
 `;
 
 export const MenuItem = styled(TouchableOpacity)`
-  background-color: ${({theme}) => theme.color.primary}36;
+  background-color: ${({ theme }) => theme.color.primary}36;
   border-radius: 17px;
   padding: 15px;
   width: 47%;
@@ -30,33 +30,42 @@ export const MenuItem = styled(TouchableOpacity)`
 `;
 
 export const MenuName = styled.Text`
-  color: ${({theme}) => theme.color.home_button_bg};
+  color: ${({ theme }) => theme.color.home_button_bg};
   font-size: ${ResponsiveSize(17)}px;
   margin-top: 10px;
+  text-align: center;
 `;
 
-export const StyledIconCronometroSvg = styled(IconCronometroSvg).attrs(({theme}) => ({
-  iconColor: theme.color.home_button_bg
-}))``;
+export const StyledIconCronometroSvg = styled(IconCronometroSvg).attrs(
+  ({ theme }) => ({
+    iconColor: theme.color.home_button_bg,
+  })
+)``;
 
-export const StyledIconTerritoriosSvg = styled(IconTerritoriosSvg).attrs(({theme}) => ({
-  iconColor: theme.color.home_button_bg
-}))``;
+export const StyledIconTerritoriosSvg = styled(IconTerritoriosSvg).attrs(
+  ({ theme }) => ({
+    iconColor: theme.color.home_button_bg,
+  })
+)``;
 
-export const StyledIconPessoasSvg = styled(IconPessoasSvg).attrs(({theme}) => ({
-  iconColor: theme.color.home_button_bg
-}))``;
+export const StyledIconPessoasSvg = styled(IconPessoasSvg).attrs(
+  ({ theme }) => ({
+    iconColor: theme.color.home_button_bg,
+  })
+)``;
 
-export const StyledIconRelatoriosSvg = styled(IconRelatoriosSvg).attrs(({theme}) => ({
-  iconColor: theme.color.home_button_bg,
-}))``;
+export const StyledIconRelatoriosSvg = styled(IconRelatoriosSvg).attrs(
+  ({ theme }) => ({
+    iconColor: theme.color.home_button_bg,
+  })
+)``;
 
 /* Landingscape styles */
 
 export const ContainerLand = styled.View`
-  background-color: ${({theme}) => theme.color.bg};
+  background-color: ${({ theme }) => theme.color.bg};
   padding: 15px;
-  height: 100%; 
+  height: 100%;
   flex-direction: row;
   justify-content: center;
   align-items: center;
@@ -64,7 +73,7 @@ export const ContainerLand = styled.View`
 `;
 
 export const MenuItemLand = styled(TouchableOpacity)`
-  background-color: ${({theme}) => theme.color.primary}36;
+  background-color: ${({ theme }) => theme.color.primary}36;
   border-radius: 17px;
   padding: 15px;
   width: 47%;
@@ -75,34 +84,42 @@ export const MenuItemLand = styled(TouchableOpacity)`
   margin: 1%;
 `;
 
-export const StyledIconCronometroSvgLand = styled(IconCronometroSvg).attrs(({theme}) => ({
-  iconColor: theme.color.home_button_bg,
-  width: 60,
-  height: 50,
-  widthVB: 67,
-  heightVB: 89,
-}))``;
+export const StyledIconCronometroSvgLand = styled(IconCronometroSvg).attrs(
+  ({ theme }) => ({
+    iconColor: theme.color.home_button_bg,
+    width: 60,
+    height: 50,
+    widthVB: 67,
+    heightVB: 89,
+  })
+)``;
 
-export const StyledIconTerritoriosSvgLand = styled(IconTerritoriosSvg).attrs(({theme}) => ({
-  iconColor: theme.color.home_button_bg,
-  width: 60,
-  height: 50,
-  widthVB: 67,
-  heightVB: 89,
-}))``;
+export const StyledIconTerritoriosSvgLand = styled(IconTerritoriosSvg).attrs(
+  ({ theme }) => ({
+    iconColor: theme.color.home_button_bg,
+    width: 60,
+    height: 50,
+    widthVB: 67,
+    heightVB: 89,
+  })
+)``;
 
-export const StyledIconPessoasSvgLand = styled(IconPessoasSvg).attrs(({theme}) => ({
-  iconColor: theme.color.home_button_bg,
-  width: 60,
-  height: 50,
-  widthVB: 67,
-  heightVB: 89,
-}))``;
+export const StyledIconPessoasSvgLand = styled(IconPessoasSvg).attrs(
+  ({ theme }) => ({
+    iconColor: theme.color.home_button_bg,
+    width: 60,
+    height: 50,
+    widthVB: 67,
+    heightVB: 89,
+  })
+)``;
 
-export const StyledIconRelatoriosSvgLand = styled(IconRelatoriosSvg).attrs(({theme}) => ({
-  iconColor: theme.color.home_button_bg,
-  width: 60,
-  height: 50,
-  widthVB: 67,
-  heightVB: 89,
-}))``;
+export const StyledIconRelatoriosSvgLand = styled(IconRelatoriosSvg).attrs(
+  ({ theme }) => ({
+    iconColor: theme.color.home_button_bg,
+    width: 60,
+    height: 50,
+    widthVB: 67,
+    heightVB: 89,
+  })
+)``;
